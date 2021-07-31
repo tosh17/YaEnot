8fa1874108fb9f8e5d4fcbea3581303a652acaf1
gorban-dev <gorban.xak@gmail.com>
2021-07-31 22:44:41 +0700

    fix navigate from SplashActivity

--- app/src/main/java/ru/yanot/practicum/presentation/splash_screen/SplashActivity.kt
+++ app/src/main/java/ru/yanot/practicum/presentation/splash_screen/SplashActivity.kt
@@ -1,9 +1,11 @@
 package ru.yanot.practicum.presentation.splash_screen
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import ru.yanot.practicum.MainActivity
 import ru.yanot.practicum.R
 
 class SplashActivity : AppCompatActivity() {
@@ -11,7 +13,10 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler(Looper.getMainLooper()).postDelayed({ finish() }, DELAY_TIME)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() }, DELAY_TIME)
     }
 
     companion object {
