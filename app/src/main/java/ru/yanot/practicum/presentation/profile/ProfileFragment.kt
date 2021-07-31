49c803771076cd506b3714fcf21ff5c2e5d75aec
tosh17 <tosh17@list.ru>
2021-07-31 18:52:34 +0300

    add front my_profile

--- app/src/main/java/ru/yanot/practicum/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/ru/yanot/practicum/presentation/profile/ProfileFragment.kt
@@ -1,8 +1,38 @@
 package ru.yanot.practicum.presentation.profile
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import coil.load
+import coil.transform.CircleCropTransformation
 import ru.yanot.practicum.R
+import ru.yanot.practicum.databinding.ProfileFragmentBinding
 
 class ProfileFragment : Fragment(R.layout.profile_fragment) {
+    private val binding: ProfileFragmentBinding by viewBinding(ProfileFragmentBinding::bind)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.body.imageAvatar.load("https://proprikol.ru/wp-content/uploads/2019/09/prikolnye-foto-enotov-15.jpg") {
+            crossfade(true)
+            //  placeholder(R.drawable.image)
+            transformations(CircleCropTransformation())
+        }
+        binding.body.userName.text = "I'm Enot, you are not"
+        val adapter = ProfileMenuAdapter {
+            Toast.makeText(context, it.toString(), Toast.LENGTH_LONG).show()
+        }
+        adapter.submitList(
+            listOf(
+                ProfileMenuItem.Notification,
+                ProfileMenuItem.Questions,
+                ProfileMenuItem.Support,
+                ProfileMenuItem.Payment,
+                ProfileMenuItem.Exit,
+            )
+        )
+        binding.body.recyclerView.adapter = adapter
 
+    }
 }
--- app/src/main/java/ru/yanot/practicum/presentation/profile/ProfileMenuAdapter.kt
+++ app/src/main/java/ru/yanot/practicum/presentation/profile/ProfileMenuAdapter.kt
@@ -1,7 +1,9 @@
 package ru.yanot.practicum.presentation.profile
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -9,8 +11,8 @@ import ru.yanot.practicum.R
 import ru.yanot.practicum.databinding.ProfileMenuItemBinding
 
 class ProfileMenuAdapter(
-    private val onClick:(ProfileMenuItem)->Unit)
-    : ListAdapter<ProfileMenuItem, ProfileMenuHolder>(CheckListDiffUtil()) {
+    private val onClick: (ProfileMenuItem) -> Unit
+) : ListAdapter<ProfileMenuItem, ProfileMenuHolder>(CheckListDiffUtil()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileMenuHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.profile_menu_item, parent, false)
@@ -38,12 +40,43 @@ class ProfileMenuHolder(
     private val binding: ProfileMenuItemBinding,
     private val onClick: (ProfileMenuItem) -> Unit
 ) :
-    RecyclerView.ViewHolder(binding.root){
-    fun onBind(item: ProfileMenuItem?) {
-
+    RecyclerView.ViewHolder(binding.root) {
+    fun onBind(item: ProfileMenuItem) {
+        binding.icon.setImageResource(item.iconId)
+        binding.text.setText(item.textId)
+        binding.array.visibility = if(item.hasArrow) View.VISIBLE else View.INVISIBLE
+        binding.separator.visibility = if(item.hasArrow) View.VISIBLE else View.INVISIBLE
+        binding.root.setOnClickListener{onClick(item)}
     }
 
 }
 
-data class ProfileMenuItem(val text:String)
+sealed class ProfileMenuItem(val iconId: Int, val textId: Int, val hasArrow : Boolean = true) {
+    object Notification : ProfileMenuItem(
+        R.drawable.ic_bell,
+        R.string.prifile_menu_notification
+    )
+    object Questions : ProfileMenuItem(
+        R.drawable.ic_menu_questions,
+        R.string.prifile_menu_questions
+    )
+    object Support : ProfileMenuItem(
+        R.drawable.ic_menu_support,
+        R.string.prifile_menu_support
+    )
+
+    object Payment : ProfileMenuItem(
+        R.drawable.ic_menu_payment,
+        R.string.prifile_menu_payment
+    )
+
+    object Exit : ProfileMenuItem(
+        R.drawable.ic_menu_exit,
+        R.string.prifile_menu_exit,
+        false
+    )
+
+
+}
+
 
