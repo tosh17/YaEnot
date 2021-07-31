a949925b02fbfddc7ca5c9b4b6cb3b0a6c70162f
Punk_Off <pankov.kirill1997@yandex.ru>
2021-07-31 17:01:44 +0300

    auth start

---
+++ app/src/main/java/ru/yanot/practicum/presentation/auth_screen/AuthDialogFragment.kt
@@ -0,0 +1,12 @@
+package ru.yanot.practicum.presentation.auth_screen
+
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class AuthDialogFragment:DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
