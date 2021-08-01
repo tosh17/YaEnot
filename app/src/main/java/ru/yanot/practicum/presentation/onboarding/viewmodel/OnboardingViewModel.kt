3922f3eb303971894e8519e5df24372b557f3671
gorban-dev <gorban.xak@gmail.com>
2021-08-01 21:57:56 +0700

    PM-4 created welcome fragment

--- app/src/main/java/ru/yanot/practicum/presentation/onboarding/viewmodel/OnboardingViewModel.kt
+++ app/src/main/java/ru/yanot/practicum/presentation/onboarding/viewmodel/OnboardingViewModel.kt
@@ -1,9 +1,12 @@
 package ru.yanot.practicum.presentation.onboarding.viewmodel
 
+import dagger.hilt.android.lifecycle.HiltViewModel
 import ru.yanot.practicum.base.BaseViewModel
 import ru.yanot.practicum.data.SharedPreferenceManager
+import javax.inject.Inject
 
-class OnboardingViewModel constructor(private val sharedPreferenceManager: SharedPreferenceManager) : BaseViewModel() {
+@HiltViewModel
+class OnboardingViewModel @Inject constructor(private val sharedPreferenceManager: SharedPreferenceManager) : BaseViewModel() {
 
     fun setOnboadingSeen() {
         sharedPreferenceManager.setOnboadingSeen()
