dd23a5586196b84b0f6f8edabf870791bb99537d
Punk_Off <pankov.kirill1997@yandex.ru>
2021-08-01 19:01:21 +0300

    auth done

--- app/src/main/java/ru/yanot/practicum/presentation/auth_screen/AuthDialogFragment.kt
+++ app/src/main/java/ru/yanot/practicum/presentation/auth_screen/AuthDialogFragment.kt
@@ -1,27 +1,32 @@
 package ru.yanot.practicum.presentation.auth_screen
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
-import android.graphics.drawable.ColorDrawable
-
-import android.graphics.Color
 import android.view.Window
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.button.MaterialButton
 import ru.yanot.practicum.R
+import ru.yanot.practicum.databinding.DialogFragmentAuthBinding
+import ru.yanot.practicum.utils.getColor
+import ru.yanot.practicum.utils.getString
+import ru.yanot.practicum.utils.loadText
 
 class AuthDialogFragment : DialogFragment() {
 
+    private val binding: DialogFragmentAuthBinding by viewBinding(DialogFragmentAuthBinding::bind)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view: View = inflater.inflate(R.layout.dialog_fragment_auth, container, false)
-        with(dialog?.window){
+        with(dialog?.window) {
             this?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             this?.requestFeature(Window.FEATURE_NO_TITLE)
         }
@@ -30,9 +35,30 @@ class AuthDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        highlightText()
         val button = view.findViewById<MaterialButton>(R.id.log_in_btn)
         button.setOnClickListener {
-            findNavController().navigate(R.id.action_authDialogFragment_to_profileFragment) }
+            findNavController().navigate(R.id.action_authDialogFragment_to_profileFragment)
+        }
+    }
+
+    private fun highlightText() {
+        val haveNotAccStr = R.string.you_havent_acc_str.getString(requireContext())
+        val signInStr = R.string.sign_in.getString(requireContext())
+
+        with(binding) {
+            signInTextView.text = String.format(haveNotAccStr, signInStr)
+            signInTextView.loadText(
+                from = String.format(haveNotAccStr, signInStr)
+                    .indexOf(signInStr),
+                to = String.format(haveNotAccStr, signInStr)
+                    .indexOf(signInStr)
+                        + String.format(signInStr).length,
+                colorInt = R.color.primary_default.getColor(requireContext()),
+                click = {
+                    //TODO add navigation
+                }
+            )
+        }
     }
 }
